47da3704899723c0971c7c17f7e78e162f12b7e4
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2008-04-20 16:25:22 -0500

    Creation of the initial schema for the database

---
+++ db/migrate/001_initial_schema.rb
@@ -0,0 +1,60 @@
+class InitialSchema < ActiveRecord::Migration
+  def self.up
+    # CAMERAS
+    create_table :cameras, :force => true do |t|
+      t.string :ip, :location, :description
+      t.string :user, :password
+      #t.string :strm_path, :strm_user, :strm_password, strm_ip # Maybe another table ??
+      t.references :codec, :quality, :agent, :null => false
+    end
+
+    # USERS
+    create_table :users, :force => true do |t|
+      t.string :login, :password, :salt, :status 
+      t.references :role
+    end
+
+    # VIDEOS
+    create_table :videos, :force => true do |t|
+      t.string :filename, :path
+      t.integer :duration
+      t.references :camera
+      t.date :date
+      t.time :start, :end
+    end
+
+    # CODECS
+    create_table :codecs, :force => true do |t|
+      t.string :name, :extension
+    end
+    
+    # AGENTS
+    create_table :agents, :force => true do |t|
+      t.string :name
+    end
+    
+    # QUALITIES
+    create_table :qualities, :force => true do |t|
+      t.string :name, :size
+      t.string :compression
+      t.integer :fps
+    end
+    
+    # ROLES
+    create_table :roles, :force => true do |t|
+      t.string :name
+      t.text :description
+    end
+
+  end
+
+  def self.down
+    drop_table :roles
+    drop_table :videos
+    drop_table :table_name
+    drop_table :agents
+    drop_table :users
+    drop_table :cameras
+    drop_table :codecs    
+  end
+end
