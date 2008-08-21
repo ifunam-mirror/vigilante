e530df55422fa206328585081fd40d7ea3205790
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2008-08-21 17:07:55 -0500

    First version of the video builder

---
+++ lib/time_range.rb
@@ -0,0 +1,36 @@
+require 'time'
+
+class Time
+  def succ
+    self + 1.minute
+  end
+end
+
+
+class Numeric
+  
+  def seconds
+    self
+  end
+  alias :second :seconds
+  
+  def minutes
+    self*60
+  end
+  alias :minute :minutes
+  
+  def hours
+    self.minutes*60
+  end
+  alias :hour :hours
+  
+  def days
+    self.hours*24
+  end
+  alias :day :days
+  
+  def ago
+    Time.now - self
+  end
+
+end
